fe0b5ff9a0f140dbeba71a9e9ab87441d2529497
mac <jeodom17@gmail.com>
2021-09-16 04:27:11 -0400

    added arrays

---
+++ Develop/script.js
@@ -0,0 +1,132 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var upperCase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var lowerCase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var characters = [
+    " ",
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+  var selections = [];
+
+  var userInput = prompt(
+    "Choose a number from 8 to 128 to determine your password length."
+  );
+  if (userInput >= 8 && userInput <= 128) {
+    var includeUpper = confirm(
+      "Would you like upper case letters in your password?"
+    );
+  } else {
+    alert("Please choose a number from 8 to 28.");
+    return null;
+  }
+  if (includeUpper === true) {
+    var includeLower = confirm(
+      "Would you like lower case letters in your password?"
+    );
+  }
+
+  // var test = confirm('quit');
+  // if (test === true) {
+  //   return null;
+  // }
+  // return(6);
+}
